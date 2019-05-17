472e8c34d6e961fd9f2cdd9cb924ee3d426598b6
TJ Draper <tj@buzzingpixel.com>
2019-05-17 14:30:20 -0500

    Add ExpressionEngine field type settings

--- src/ExpressionEngine/ft.cast.php
+++ src/ExpressionEngine/ft.cast.php
@@ -17,6 +17,141 @@ class Cast_ft extends EE_Fieldtype
         'version' => Constants::VERSION,
     ];
 
+    // phpcs:disable SlevomatCodingStandard.TypeHints.TypeHintDeclaration.MissingParameterTypeHint
+
+    /**
+     * @param mixed[]|null $data
+     *
+     * @return mixed[]
+     */
+    public function display_settings($data) : array
+    {
+        $data = is_array($data) ? $data : [];
+
+        $useFtp = $data['cast_use_ftp'] ?? 'n';
+
+        $ftpProtocol = $data['cast_ftp_protocol'] ?? 'ftp';
+
+        return [
+            'standard_field_options' => [
+                'label' => 'field_options',
+                'group' => 'cast',
+                'settings' => [
+                    'cast_upload_path' => [
+                        'title' => 'upload_path',
+                        'fields' => [
+                            'cast_upload_path' => [
+                                'type' => 'text',
+                                'required' => true,
+                                'value' => $data['cast_upload_path'] ?? '',
+                            ],
+                        ],
+                    ],
+                    'cast_upload_url' => [
+                        'title' => 'upload_url',
+                        'desc' => 'upload_url_explain',
+                        'fields' => [
+                            'cast_upload_url' => [
+                                'type' => 'text',
+                                'required' => true,
+                                'value' => $data['cast_upload_url'] ?? '',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'ftp_field_options' => [
+                'label' => 'ftp_field_options',
+                'group' => 'cast',
+                'settings' => [
+                    'cast_use_ftp' => [
+                        'title' => 'use_ftp',
+                        'fields' => [
+                            'cast_use_ftp' => [
+                                'type' => 'yes_no',
+                                'value' => $useFtp === 'y' ? 'y' : 'n',
+                            ],
+                        ],
+                    ],
+                    'cast_ftp_protocol' => [
+                        'title' => 'protocol',
+                        'fields' => [
+                            'cast_ftp_protocol' => [
+                                'type' => 'inline_radio',
+                                'choices' => [
+                                    'ftp' => 'FTP',
+                                    'sftp' => 'SFTP',
+                                ],
+                                'value' => $ftpProtocol,
+                            ],
+                        ],
+                    ],
+                    'cast_upload_ftp_server' => [
+                        'title' => 'upload_ftp_server',
+                        'fields' => [
+                            'cast_upload_ftp_server' => [
+                                'type' => 'text',
+                                'value' => $data['cast_upload_ftp_server'] ?? '',
+                            ],
+                        ],
+                    ],
+                    'cast_upload_ftp_user_name' => [
+                        'title' => 'upload_ftp_user_name',
+                        'desc' => 'upload_ftp_user_name_explain',
+                        'fields' => [
+                            'cast_upload_ftp_user_name' => [
+                                'type' => 'text',
+                                'value' => $data['cast_upload_ftp_user_name'] ?? '',
+                            ],
+                        ],
+                    ],
+                    'cast_upload_ftp_password' => [
+                        'title' => 'upload_ftp_password',
+                        'desc' => 'upload_ftp_password_explain',
+                        'fields' => [
+                            'cast_upload_ftp_password' => [
+                                'type' => 'text',
+                                'value' => $data['cast_upload_ftp_password'] ?? '',
+                            ],
+                        ],
+                    ],
+                    'cast_upload_ftp_port' => [
+                        'title' => 'upload_ftp_port',
+                        'desc' => 'upload_ftp_port_explain',
+                        'fields' => [
+                            'cast_upload_ftp_port' => [
+                                'type' => 'text',
+                                'value' => $data['cast_upload_ftp_port'] ?? '',
+                            ],
+                        ],
+                    ],
+                    'cast_upload_ftp_remote_path' => [
+                        'title' => 'upload_ftp_remote_path',
+                        'desc' => 'upload_ftp_remote_path_explain',
+                        'fields' => [
+                            'cast_upload_ftp_remote_path' => [
+                                'type' => 'text',
+                                'value' => $data['cast_upload_ftp_remote_path'] ?? '',
+                            ],
+                        ],
+                    ],
+                    'cast_upload_ftp_remote_url' => [
+                        'title' => 'upload_ftp_remote_url',
+                        'desc' => 'upload_ftp_remote_url_explain',
+                        'fields' => [
+                            'cast_upload_ftp_remote_url' => [
+                                'type' => 'text',
+                                'value' => $data['cast_upload_ftp_remote_url'] ?? '',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    // phpcs:enable SlevomatCodingStandard.TypeHints.TypeHintDeclaration.MissingParameterTypeHint
+
     /**
      * @param mixed $data
      */
--- tests/ExpressionEngine/CastFtTest.php
+++ tests/ExpressionEngine/CastFtTest.php
@@ -29,6 +29,253 @@ class CastFtTest extends TestCase
         );
     }
 
+    public function testDisplaySettingsWithNoInputData() : void
+    {
+        self::assertEquals(
+            [
+                'standard_field_options' => [
+                    'label' => 'field_options',
+                    'group' => 'cast',
+                    'settings' => [
+                        'cast_upload_path' => [
+                            'title' => 'upload_path',
+                            'fields' => [
+                                'cast_upload_path' => [
+                                    'type' => 'text',
+                                    'required' => true,
+                                    'value' => '',
+                                ],
+                            ],
+                        ],
+                        'cast_upload_url' => [
+                            'title' => 'upload_url',
+                            'fields' => [
+                                'cast_upload_url' => [
+                                    'type' => 'text',
+                                    'required' => true,
+                                    'value' => '',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                'ftp_field_options' => [
+                    'label' => 'ftp_field_options',
+                    'group' => 'cast',
+                    'settings' => [
+                        'cast_use_ftp' => [
+                            'title' => 'use_ftp',
+                            'fields' => [
+                                'cast_use_ftp' => [
+                                    'type' => 'yes_no',
+                                    'value' => 'n',
+                                ],
+                            ],
+                        ],
+                        'cast_ftp_protocol' => [
+                            'title' => 'protocol',
+                            'fields' => [
+                                'cast_ftp_protocol' => [
+                                    'type' => 'inline_radio',
+                                    'choices' => [
+                                        'ftp' => 'FTP',
+                                        'sftp' => 'SFTP',
+                                    ],
+                                    'value' => 'ftp',
+                                ],
+                            ],
+                        ],
+                        'cast_upload_ftp_server' => [
+                            'title' => 'upload_ftp_server',
+                            'fields' => [
+                                'cast_upload_ftp_server' => [
+                                    'type' => 'text',
+                                    'value' => '',
+                                ],
+                            ],
+                        ],
+                        'cast_upload_ftp_user_name' => [
+                            'title' => 'upload_ftp_user_name',
+                            'fields' => [
+                                'cast_upload_ftp_user_name' => [
+                                    'type' => 'text',
+                                    'value' => '',
+                                ],
+                            ],
+                        ],
+                        'cast_upload_ftp_password' => [
+                            'title' => 'upload_ftp_password',
+                            'fields' => [
+                                'cast_upload_ftp_password' => [
+                                    'type' => 'text',
+                                    'value' => '',
+                                ],
+                            ],
+                        ],
+                        'cast_upload_ftp_port' => [
+                            'title' => 'upload_ftp_port',
+                            'fields' => [
+                                'cast_upload_ftp_port' => [
+                                    'type' => 'text',
+                                    'value' => '',
+                                ],
+                            ],
+                        ],
+                        'cast_upload_ftp_remote_path' => [
+                            'title' => 'upload_ftp_remote_path',
+                            'fields' => [
+                                'cast_upload_ftp_remote_path' => [
+                                    'type' => 'text',
+                                    'value' => '',
+                                ],
+                            ],
+                        ],
+                        'cast_upload_ftp_remote_url' => [
+                            'title' => 'upload_ftp_remote_url',
+                            'fields' => [
+                                'cast_upload_ftp_remote_url' => [
+                                    'type' => 'text',
+                                    'value' => '',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            $this->ft->display_settings(null)
+        );
+    }
+
+    public function testDisplaySettingsWithInputData() : void
+    {
+        $data = [
+            'cast_upload_path' => 'uploadPathTest',
+            'cast_upload_url' => 'uploadUrlTest',
+            'cast_use_ftp' => 'y',
+            'cast_ftp_protocol' => 'sftp',
+            'cast_upload_ftp_server' => 'uploadFtpServerTest',
+            'cast_upload_ftp_user_name' => 'uploadFtpUserNameTest',
+            'cast_upload_ftp_password' => 'uploadFtpPasswordTest',
+            'cast_upload_ftp_port' => 'uploadFtpPortTest',
+            'cast_upload_ftp_remote_path' => 'uploadFtpRemotePathTest',
+            'cast_upload_ftp_remote_url' => 'uploadFtpRemoteUrlTest',
+        ];
+
+        self::assertEquals(
+            [
+                'standard_field_options' => [
+                    'label' => 'field_options',
+                    'group' => 'cast',
+                    'settings' => [
+                        'cast_upload_path' => [
+                            'title' => 'upload_path',
+                            'fields' => [
+                                'cast_upload_path' => [
+                                    'type' => 'text',
+                                    'required' => true,
+                                    'value' => $data['cast_upload_path'],
+                                ],
+                            ],
+                        ],
+                        'cast_upload_url' => [
+                            'title' => 'upload_url',
+                            'fields' => [
+                                'cast_upload_url' => [
+                                    'type' => 'text',
+                                    'required' => true,
+                                    'value' => $data['cast_upload_url'],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                'ftp_field_options' => [
+                    'label' => 'ftp_field_options',
+                    'group' => 'cast',
+                    'settings' => [
+                        'cast_use_ftp' => [
+                            'title' => 'use_ftp',
+                            'fields' => [
+                                'cast_use_ftp' => [
+                                    'type' => 'yes_no',
+                                    'value' => 'y',
+                                ],
+                            ],
+                        ],
+                        'cast_ftp_protocol' => [
+                            'title' => 'protocol',
+                            'fields' => [
+                                'cast_ftp_protocol' => [
+                                    'type' => 'inline_radio',
+                                    'choices' => [
+                                        'ftp' => 'FTP',
+                                        'sftp' => 'SFTP',
+                                    ],
+                                    'value' => 'sftp',
+                                ],
+                            ],
+                        ],
+                        'cast_upload_ftp_server' => [
+                            'title' => 'upload_ftp_server',
+                            'fields' => [
+                                'cast_upload_ftp_server' => [
+                                    'type' => 'text',
+                                    'value' => $data['cast_upload_ftp_server'],
+                                ],
+                            ],
+                        ],
+                        'cast_upload_ftp_user_name' => [
+                            'title' => 'upload_ftp_user_name',
+                            'fields' => [
+                                'cast_upload_ftp_user_name' => [
+                                    'type' => 'text',
+                                    'value' => $data['cast_upload_ftp_user_name'],
+                                ],
+                            ],
+                        ],
+                        'cast_upload_ftp_password' => [
+                            'title' => 'upload_ftp_password',
+                            'fields' => [
+                                'cast_upload_ftp_password' => [
+                                    'type' => 'text',
+                                    'value' => $data['cast_upload_ftp_password'],
+                                ],
+                            ],
+                        ],
+                        'cast_upload_ftp_port' => [
+                            'title' => 'upload_ftp_port',
+                            'fields' => [
+                                'cast_upload_ftp_port' => [
+                                    'type' => 'text',
+                                    'value' => $data['cast_upload_ftp_port'],
+                                ],
+                            ],
+                        ],
+                        'cast_upload_ftp_remote_path' => [
+                            'title' => 'upload_ftp_remote_path',
+                            'fields' => [
+                                'cast_upload_ftp_remote_path' => [
+                                    'type' => 'text',
+                                    'value' => $data['cast_upload_ftp_remote_path'],
+                                ],
+                            ],
+                        ],
+                        'cast_upload_ftp_remote_url' => [
+                            'title' => 'upload_ftp_remote_url',
+                            'fields' => [
+                                'cast_upload_ftp_remote_url' => [
+                                    'type' => 'text',
+                                    'value' => $data['cast_upload_ftp_remote_url'],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            $this->ft->display_settings($data)
+        );
+    }
+
     public function testDisplayField() : void
     {
         self::assertSame(
