93a6b51f4f072d07fe7ffe2cc72262e0f9327c98
TJ Draper <tj@buzzingpixel.com>
2019-05-17 15:30:46 -0500

    Fix up PHPStan

--- phpstanBootstrap.php
+++ phpstanBootstrap.php
@@ -1,3 +1,13 @@
 <?php
 
 declare(strict_types=1);
+
+define('PATH_THIRD', 'pathThirdTest');
+
+/**
+ * @return mixed
+ */
+function ee()
+{
+    throw new RuntimeException("We can't test ee() so we should not call it directly");
+}
