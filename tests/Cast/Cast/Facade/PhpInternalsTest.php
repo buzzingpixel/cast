53032c9e598ee9cc7754c3159670d077ce075518
TJ Draper <tj@buzzingpixel.com>
2019-05-20 14:23:01 -0500

    Add PhpInternals Facade test coverage

---
+++ tests/Cast/Cast/Facade/PhpInternalsTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Cast\Cast\Facade;
+
+use BuzzingPixel\Cast\Cast\Facade\PhpInternals;
+use PHPUnit\Framework\TestCase;
+
+class PhpInternalsTest extends TestCase
+{
+    public function testIsDir() : void
+    {
+        $phpInternals = new PhpInternals();
+
+        self::assertTrue($phpInternals->isDir(TESTS_BASE_PATH . '/filesystemTesting'));
+
+        self::assertFalse($phpInternals->isDir('foo'));
+    }
+}
