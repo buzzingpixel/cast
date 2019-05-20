6fffd8464a77dc4fb6ce369933540e92dd5e80d4
TJ Draper <tj@buzzingpixel.com>
2019-05-20 14:34:08 -0500

    Add NormalizePaths service test

---
+++ tests/Cast/ExpressionEngine/Service/NormalizePathsTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Cast\ExpressionEngine\Service;
+
+use BuzzingPixel\Cast\ExpressionEngine\Service\NormalizePaths;
+use EE_Config;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Throwable;
+
+class NormalizePathsTest extends TestCase
+{
+    /**
+     * @throws Throwable
+     */
+    public function testNormalize() : void
+    {
+        /** @var MockObject&EE_Config $eeConfig */
+        $eeConfig = $this->createMock(EE_Config::class);
+
+        $eeConfig->method('item')
+            ->willReturnCallback(static function ($item) {
+                switch ($item) {
+                    case 'base_url':
+                        return 'https://foobar.com/';
+
+                        break;
+                    case 'base_path':
+                        return '/base/path/foo/bar/';
+
+                        break;
+                    default:
+                        return null;
+                }
+            });
+
+        $normalizePaths = new NormalizePaths($eeConfig);
+
+        self::assertSame(
+            'https://foobar.com/baz/base/path/foo/bar/bap',
+            $normalizePaths->normalize('{base_url}/baz{base_path}/bap')
+        );
+    }
+}
