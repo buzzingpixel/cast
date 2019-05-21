be33d44e672f03ca43363843eba131fbfa9fd0e4
TJ Draper <tj@buzzingpixel.com>
2019-05-21 12:07:04 -0500

    Add styling for Cast Audio Field

---
+++ src/Templates/Components/WaitingAnimation.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+// phpcs:disable Generic.Files.InlineHTML.Found
+?>
+
+<div class="WaitingAnimation">
+    <div class="WaitingAnimation__Bounce WaitingAnimation__Bounce--One"></div>
+    <div class="WaitingAnimation__Bounce WaitingAnimation__Bounce--Two"></div>
+    <div class="WaitingAnimation__Bounce WaitingAnimation__Bounce--Three"></div>
+</div>
